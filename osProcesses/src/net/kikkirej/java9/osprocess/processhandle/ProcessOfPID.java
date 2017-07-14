d227121dbb9c6c581d84a8bc841390ff09ad1ca5
Fabian Lemke <info@kikkirej.net>
2017-07-14 22:53:29 +0200

    of() add from ProcessHandle

---
+++ osProcesses/src/net/kikkirej/java9/osprocess/processhandle/ProcessOfPID.java
@@ -0,0 +1,23 @@
+package net.kikkirej.java9.osprocess.processhandle;
+
+import java.util.Optional;
+
+/**
+ * shows how to get a process of a specific PID
+ * @author Kikkirej
+ * @since 14.07.2017
+ */
+public class ProcessOfPID {
+
+	public static void main(String[] args) {
+		new ProcessOfPID().run();
+	}
+
+	private void run() {
+		long pid = ProcessHandle.current().pid();
+		Optional<ProcessHandle> processOfPid = ProcessHandle.of(pid);
+		ProcessHandle processHandle = processOfPid.get();
+		System.out.println(processHandle.info().toString());
+	}
+
+}
